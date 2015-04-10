1a9c7b4f21130b3c98a145cf113834a394f8d28c
Stephanie Lo <lo.stephaniec@gmail.com>
2015-04-10 19:25:55 -0400

    new comments will add

--- the-stack-overflow/app/controllers/comments_controller.rb
+++ the-stack-overflow/app/controllers/comments_controller.rb
@@ -4,7 +4,18 @@ class CommentsController < ApplicationController
 
   end
 
-  def post
+  def create
+    @comment = Comment.new(question_params)
+    if @comment.save
+      redirect_to @comment.commentable
+    else
+      [500, "UHOH! ERROR!"]
+    end
+  end
 
+  private
+
+  def question_params
+    params.require(:comments).permit(:body)
   end
 end
