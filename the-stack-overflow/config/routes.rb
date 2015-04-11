add59a5fae961af9715bf27c3a8016a0fbfd8d72
Angel Baek <angelbaek1@gmail.com>
2015-04-10 20:44:14 -0400

    fixed routes for signup. need to dry out forms for login/signup

--- the-stack-overflow/config/routes.rb
+++ the-stack-overflow/config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   get 'logout'  => 'sessions#destroy'
+  get 'signup' => 'users#new'
+  post 'signup' => 'users#create'
   resources :sessions, only: [:new, :create, :destroy]
   resources :questions do
     resources :answers
