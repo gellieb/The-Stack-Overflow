5f2d87716a32e7ce5c9de7b95fd87189858b957f
Notika314 <notika314@gmail.com>
2015-04-11 20:30:57 -0400

    edit

--- the-stack-overflow/app/models/question.rb
+++ the-stack-overflow/app/models/question.rb
@@ -4,10 +4,7 @@ class Question < ActiveRecord::Base
   has_many :comments, as: :commentable
   has_many :votes, as: :votable
 
-<<<<<<< HEAD
-  # def self.sort_by_votes
-  #   order('votes asc')
-  # end
+
   def best_answer
     self.answers.find_by(best: true)
   end
@@ -15,23 +12,5 @@ class Question < ActiveRecord::Base
     best = self.answers.delete(best_answer)
     self.answers.unshift(best)
   end
-=======
-
-  # def self.sort_by_votes
-  #   order('votes asc')
-  # end
-
-  def best_answer
-    self.answers.find_by(best: true)
-  end
-<<<<<<< HEAD
-
->>>>>>>  add question count to view, use pluralize method
-=======
-  def answer_sort
-    best = self.answers.delete(best_answer)
-    self.answers.unshift(best)
-  end
->>>>>>> edit
 
 end
